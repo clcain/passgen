a48094243cc74a7e49b0f94c1f5857f78337737f
Chandler Cain <chandler@clcain.com>
2017-02-08 03:44:01 -0500

    Add difficulty shortcut parameter
    Add a parameter that may be used to specify the difficulty
    of the password config.

--- src/passgen.py
+++ src/passgen.py
@@ -9,12 +9,46 @@ from password_config import PasswordConfig
 def main():
     config = PasswordConfig()
     try:
-        config.upper_count = int(sys.argv[1])
-        config.lower_count = int(sys.argv[2])
-        config.num_count = int(sys.argv[3])
-        config.special_count = int(sys.argv[4])
-        config.toggle_hand = bool(int(sys.argv[5]))
-        config.allow_doubles = bool(int(sys.argv[6]))
+        if sys.argv[1] == 'easy':
+            config.upper_count = 2
+            config.lower_count = 4
+            config.num_count = 2
+            config.special_count = 0
+            config.toggle_hand = True
+            config.allow_doubles = False
+
+        elif sys.argv[1] == 'medium':
+            config.upper_count = 4
+            config.lower_count = 8
+            config.num_count = 4
+            config.special_count = 0
+            config.toggle_hand = True
+            config.allow_doubles = False
+
+        elif sys.argv[1] == 'hard':
+            config.upper_count = 8
+            config.lower_count = 12
+            config.num_count = 8
+            config.special_count = 4
+            config.toggle_hand = True
+            config.allow_doubles = False
+
+        elif sys.argv[1] == 'insane':
+            config.upper_count = 16
+            config.lower_count = 24
+            config.num_count = 16
+            config.special_count = 8
+            config.toggle_hand = True
+            config.allow_doubles = False
+
+        else:
+            config.upper_count = int(sys.argv[1])
+            config.lower_count = int(sys.argv[2])
+            config.num_count = int(sys.argv[3])
+            config.special_count = int(sys.argv[4])
+            config.toggle_hand = bool(int(sys.argv[5]))
+            config.allow_doubles = bool(int(sys.argv[6]))
+
     except IndexError, ValueError:
         pass
     sys.stderr.write('{0}\n'.format(config))
