29c05c4a59a5db7a56b532b35e632d3c4b9295c3
Chandler Cain <chandler@clcain.com>
2018-11-15 19:03:14 -0500

    Use argparse

--- src/passgen.py
+++ src/passgen.py
@@ -1,58 +1,36 @@
-#! /usr/bin/python
+#!/usr/bin/env python3
 
+import argparse
 import sys
 
 import password_generator
 
 from password_config import PasswordConfig
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('difficulty', default=None)
+    parser.add_argument('--upper', type=int, default=None)
+    parser.add_argument('--lower', type=int, default=None)
+    parser.add_argument('--special', type=int, default=None)
+    parser.add_argument('--toggle', action='store_true')
+    parser.add_argument('--duplicates', action='store_true')
+    return parser.parse_args()
+
 def main():
-    config = PasswordConfig()
-    try:
-        if sys.argv[1] == 'easy':
-            config.upper_count = 2
-            config.lower_count = 4
-            config.num_count = 2
-            config.special_count = 0
-            config.toggle_hand = True
-            config.allow_doubles = False
-
-        elif sys.argv[1] == 'medium':
-            config.upper_count = 4
-            config.lower_count = 8
-            config.num_count = 4
-            config.special_count = 0
-            config.toggle_hand = True
-            config.allow_doubles = False
-
-        elif sys.argv[1] == 'hard':
-            config.upper_count = 8
-            config.lower_count = 12
-            config.num_count = 8
-            config.special_count = 4
-            config.toggle_hand = False
-            config.allow_doubles = True
-
-        elif sys.argv[1] == 'insane':
-            config.upper_count = 16
-            config.lower_count = 24
-            config.num_count = 16
-            config.special_count = 8
-            config.toggle_hand = False
-            config.allow_doubles = True
-
-        else:
-            config.upper_count = int(sys.argv[1])
-            config.lower_count = int(sys.argv[2])
-            config.num_count = int(sys.argv[3])
-            config.special_count = int(sys.argv[4])
-            config.toggle_hand = bool(int(sys.argv[5]))
-            config.allow_doubles = bool(int(sys.argv[6]))
-
-    except IndexError, ValueError:
-        pass
-    sys.stderr.write('{0}\n'.format(config))
+    if len(sys.argv) > 1:
+        args = parse_args()
+        config = PasswordConfig(difficulty=args.difficulty,
+                                upper_count=args.upper,
+                                lower_count=args.lower,
+                                special_count=args.special,
+                                toggle_hand=args.toggle,
+                                allow_doubles=args.duplicates)
+    else:
+        config = PasswordConfig()
+    sys.stderr.write('{}\n'.format(config))
     password = password_generator.generate_password(config)
-    print password
+    print(password)
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
